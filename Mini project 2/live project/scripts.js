df3420f431b754fdb90ca192cd9c6f0ce83831ea
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-28 10:40:14 -0400

    added provided starter code to the project

---
+++ Mini project 2/live project/scripts.js
@@ -0,0 +1,20 @@
+// dependencies 
+// start button 
+// reset button
+// countdown
+// wins 
+// loses 
+// word display
+
+
+// data 
+// wins
+//loses
+//wordbank
+//time left
+// functions
+
+
+// user intereactions
+// a user type a key 
+document.addEventListener("keypress",handllekeypress)
